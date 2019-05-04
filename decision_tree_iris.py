e79b034da5f5ba0f204462af0e4d375f75587d84
RISHAB JAIN <36708743+rishabjn@users.noreply.github.com>
2019-05-04 16:48:41 +0530

    Update decision_tree_iris.py

--- decision_tree_iris.py
+++ decision_tree_iris.py
@@ -6,20 +6,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 
-iris_data = pd.read_csv("iris.csv")
+iris_data = pd.read_csv("iris.csv") #Reading data using pandas
 
-featues = iris_data[["SepalLength","SepalWidth","PetalLength","PetalWidth"]]
+featues = iris_data[["SepalLength","SepalWidth","PetalLength","PetalWidth"]] #Selecting features for training
 
-target_var = iris_data.Class
+target_var = iris_data.Class   #selecting target class
 
-ftrain, ftest, ttrain,ttest = train_test_split(featues,target_var,test_size=0.2)
+ftrain, ftest, ttrain,ttest = train_test_split(featues,target_var,test_size=0.2)  #splitting dataset into train set and test set
 
-model = DecisionTreeClassifier()
+model = DecisionTreeClassifier() # Running the Model 
 
-fitted_model = model.fit(ftrain, ttrain)
+fitted_model = model.fit(ftrain, ttrain) #fitting on the training dataset
 
 
-prediction = fitted_model.predict(ftest)
+prediction = fitted_model.predict(ftest) #prediction on test dataset
 
-print(confusion_matrix(ttest,prediction))
+print(confusion_matrix(ttest,prediction))  #Printing confusion matrix and accuracy 
 print(accuracy_score(ttest, prediction))
